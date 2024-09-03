8b3ef6f2621dae8d758444e4c4891cc400cf5ffd
peerawitsave <peerawit2010@gmail.com>
2024-09-03 21:45:17 +0700

    CameraController and CameraRotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,54 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Camera> cameras;
+    private int currentCameraIndex = 0;
+
     void Start()
     {
         
+        for (int i = 0; i < cameras.Count; i++)
+        {
+            cameras[i].gameObject.SetActive(i == currentCameraIndex);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SwitchCamera(1);
+        }
+
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SwitchCamera(-1);
+        }
+    }
+
+    void SwitchCamera(int direction)
+    {
+       
+        cameras[currentCameraIndex].gameObject.SetActive(false);
+
+        
+        currentCameraIndex += direction;
+
+        
+        if (currentCameraIndex >= cameras.Count)
+        {
+            currentCameraIndex = 0; 
+        }
+        else if (currentCameraIndex < 0)
+        {
+            currentCameraIndex = cameras.Count - 1; 
+        }
+
+       
+        cameras[currentCameraIndex].gameObject.SetActive(true);
     }
 }
